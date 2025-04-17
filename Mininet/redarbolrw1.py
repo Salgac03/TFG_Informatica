481bdaa17aeb58965a85a3a5bcf546a1c8d289e3
alberto <alcanas2002@gmail.com>
2025-04-17 21:15:52 +0200

    Medidas con Iperf

--- Mininet/redarbolrw1.py
+++ Mininet/redarbolrw1.py
@@ -9,13 +9,13 @@ class SimpleTopo(Topo):
     """
     Topología simple compuesta por:
       - hsrc: Host origen con IP 10.0.1.1/24 y MAC constante
-      - hdst: Host destino con IP 10.0.2.1/24 y MAC constante
+      - hdst: Host destino con IP 10.0.1.2/24 y MAC constante
       - s1: Switch conectado a ambos hosts
     """
     def build(self):
         # Crear hosts con direcciones MAC constantes
         hsrc = self.addHost('hsrc', ip='10.0.1.1/24', mac='00:00:00:00:01:01')
-        hdst = self.addHost('hdst', ip='10.0.2.1/24', mac='00:00:00:00:02:02')
+        hdst = self.addHost('hdst', ip='10.0.1.2/24', mac='00:00:00:00:02:02')
 
         # Crear switch
         s1 = self.addSwitch('s1')
@@ -36,7 +36,7 @@ def run():
 
     # Realizar un ping de verificación desde hsrc a hdst
     print("Ping test from hsrc to hdst:")
-    print(net['hsrc'].cmd('ping -c 3 10.0.2.1'))
+    print(net['hsrc'].cmd('ping -c 3 10.0.1.2'))
 
     # Abrir la CLI para pruebas adicionales
     CLI(net)
